f99895c0b9aa5c65c402ef3abcc00b70c503749a
zerogpm <zerogpm@gmail.com>
2022-05-03 21:07:07 -0700

    fix issues

--- data/hangman/main.py
+++ data/hangman/main.py
@@ -1,17 +1,14 @@
 import random
 import hangman_art
-import word_list
+from word_list import word_list
 #Step 1
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-chosen_word = word_list.word_list[random.randint(0, len(word_list.word_list) - 1)]
+chosen_word = word_list[random.randint(0, len(word_list) - 1)]
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 display = []
 print(hangman_art.logo)
 count_word_choose = len(chosen_word)
@@ -29,17 +26,20 @@ while not end_of_the_game:
         if letter == guessed:
             display[position] = letter
 
-    if guess not in chosen_word:
-        player_life -= 1
-        if player_life == 0:
-            print("you lose")
-            end_of_the_game = True
-
     if "_" not in display:
         end_of_the_game = True
         print("You win")
 
     print(display)
+
+    if guess not in chosen_word:
+        print(f"you guessed {guess}, that is not in the word, you lose a life.")
+        player_life -= 1
+        if player_life == 0:
+            print("you lose")
+            print(f"the choose word is {chosen_word}")
+            end_of_the_game = True
+
     print(hangman_art.stages[player_life])
 
     if guess in chosen_word:
