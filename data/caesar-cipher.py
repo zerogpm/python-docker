060421ba26da88d8744b618d99eb189b489ec603
zerogpm <zerogpm@gmail.com>
2022-05-05 21:37:08 -0700

    caesar

--- data/caesar-cipher.py
+++ data/caesar-cipher.py
@@ -1,10 +1,11 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
 def encrypt(plain_text, shift_amount):
     encrypt_text = []
     for char in plain_text:
@@ -15,4 +16,19 @@ def encrypt(plain_text, shift_amount):
         encrypt_text.append(alphabet[new_position])
     return encrypt_text
 
-print(encrypt(plain_text=text, shift_amount=shift))
+
+def decrypt(encrypt_text, shift_amount):
+    decrypt_text = []
+    for char in encrypt_text:
+        position = alphabet.index(char)
+        new_position = position - shift_amount
+        if new_position > len(alphabet) - 1:
+            new_position = new_position - len(alphabet)
+        decrypt_text.append(alphabet[new_position])
+    return decrypt_text
+
+
+if direction == "encode":
+    print(encrypt(plain_text=text, shift_amount=shift))
+else:
+    print(decrypt(encrypt_text=text, shift_amount=shift))
